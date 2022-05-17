080ef752c1ea89e15bd5850857d9e4c812bb6fac
SetonRoboticsMFoor <48261500+SetonRoboticsMFoor@users.noreply.github.com>
2022-05-17 14:49:52 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+while (true) {
+    var people = [""]
+    var tempName = prompt("Enter a name", "Type name here")
+
+    // pop() will remove an array element, push() will add one
+    people.push(tempName);
+    alert(people);
+}
