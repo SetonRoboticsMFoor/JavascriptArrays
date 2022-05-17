1a1d0ce4da75f0f528ea2ae2feef255143de3389
SetonRoboticsMFoor <48261500+SetonRoboticsMFoor@users.noreply.github.com>
2022-05-17 15:06:56 -0700

    updated to make it work

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+var people = [];
+
 while (true) {
-    var people = [""]
-    var tempName = prompt("Enter a name", "Type name here")
+
+    var tempName = prompt("Enter a name", "Type name here");
 
     // pop() will remove an array element, push() will add one
     people.push(tempName);
     alert(people);
+
+    // shift() removes the first element
+    // unshift() adds elements to the beginning
+    // splice() adds elements and then removes
+    // splice(1,2,"mary") would add Mary after index 1 and remove 2 elements after it
+    // the second argument can be set to 0 if you do not want to remove anything
 }
